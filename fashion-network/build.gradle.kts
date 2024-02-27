f8c302cb5deed880994d6cbe4aed3c5e229aaae1
MishkaV <mavorozhtsov_1@edu.hse.ru>
2024-02-27 16:20:21 +0300

    Some todo of ktor client

--- fashion-network/build.gradle.kts
+++ fashion-network/build.gradle.kts
@@ -6,13 +6,16 @@ plugins {
     alias(libs.plugins.kotlinx.serialization)
 }
 
-androidLibrary(name = "com.aiuta.fashionsdk.network")
+androidLibrary(
+    name = "com.aiuta.fashionsdk.network",
+    config = true,
+)
 
 dependencies {
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.ktor.core)
     implementation(libs.ktor.engine.okhttp)
-    implementation(libs.ktor.logging) // TODO DELETE
+    implementation(libs.ktor.logging)
     implementation(libs.ktor.negotiation)
     implementation(libs.ktor.serialization)
 
