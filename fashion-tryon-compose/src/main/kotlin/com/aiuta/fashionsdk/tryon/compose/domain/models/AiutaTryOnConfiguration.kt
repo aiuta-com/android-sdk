2824c15ad561b9b2f9c06c92e47cece6331f805b
MishkaV <mavorozhtsov_1@edu.hse.ru>
2024-05-30 10:58:05 +0200

    Add optional wish list flag

--- fashion-tryon-compose/src/main/kotlin/com/aiuta/fashionsdk/tryon/compose/domain/models/AiutaTryOnConfiguration.kt
+++ fashion-tryon-compose/src/main/kotlin/com/aiuta/fashionsdk/tryon/compose/domain/models/AiutaTryOnConfiguration.kt
@@ -11,6 +11,14 @@ import com.aiuta.fashionsdk.tryon.compose.ui.AiutaTryOnFlow
 @Immutable
 public interface AiutaTryOnConfiguration {
     /**
+     * Language of Aiuta Try on Flow
+     *
+     * By default, we will look for the language of the device - if SDK supports it,
+     * we will use it, otherwise we will use English.
+     */
+    public val language: AiutaTryOnLanguage?
+
+    /**
      * The maximum number of photos that a user can select
      * in the system picket for virtual try on.
      */
@@ -26,22 +34,21 @@ public interface AiutaTryOnConfiguration {
     public val isHistoryAvailable: Boolean
 
     /**
-     * Language of Aiuta Try on Flow
-     *
-     * By default, we will look for the language of the device - if SDK supports it,
-     * we will use it, otherwise we will use English.
+     * Flag which turn on or off possibility to use add to wishlist option
      */
-    public val language: AiutaTryOnLanguage?
+    public val isWishlistAvailable: Boolean
 }
 
 public fun defaultAiutaTryOnConfiguration(
+    language: AiutaTryOnLanguage? = null,
     photoSelectionLimit: Int = 10,
     isHistoryAvailable: Boolean = true,
-    language: AiutaTryOnLanguage? = null,
+    isWishlistAvailable: Boolean = true,
 ): AiutaTryOnConfiguration {
     return object : AiutaTryOnConfiguration {
+        override val language: AiutaTryOnLanguage? = language
         override val photoSelectionLimit: Int = photoSelectionLimit
         override val isHistoryAvailable: Boolean = isHistoryAvailable
-        override val language: AiutaTryOnLanguage? = language
+        override val isWishlistAvailable: Boolean = isWishlistAvailable
     }
 }
