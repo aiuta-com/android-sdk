63435a18f6db50f46cc1ed74f20c76223eb577cf
MishkaV <mavorozhtsov_1@edu.hse.ru>
2024-10-04 14:49:22 +0200

    Add zoom for sku images

--- fashion-tryon-compose/src/main/kotlin/com/aiuta/fashionsdk/tryon/compose/ui/internal/screens/result/components/footer/blocks/photos/ItemPhotosBlock.kt
+++ fashion-tryon-compose/src/main/kotlin/com/aiuta/fashionsdk/tryon/compose/ui/internal/screens/result/components/footer/blocks/photos/ItemPhotosBlock.kt
@@ -13,18 +13,31 @@ import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyGridScope
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInRoot
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.toSize
 import coil.compose.SubcomposeAsyncImage
 import coil.request.ImageRequest
 import com.aiuta.fashionsdk.compose.tokens.composition.LocalTheme
+import com.aiuta.fashionsdk.compose.tokens.utils.clickableUnindicated
+import com.aiuta.fashionsdk.tryon.compose.domain.models.internal.zoom.ZoomImageUiModel
 import com.aiuta.fashionsdk.tryon.compose.ui.internal.components.progress.LoadingProgress
 import com.aiuta.fashionsdk.tryon.compose.ui.internal.controller.composition.LocalController
 import com.aiuta.fashionsdk.tryon.compose.ui.internal.screens.result.components.footer.FOOTER_FULL_SIZE_SPAN
+import com.aiuta.fashionsdk.tryon.compose.ui.internal.screens.zoom.controller.openZoomImageScreen
 
 internal fun LazyGridScope.itemPhotosBlock(modifier: Modifier = Modifier) {
     item(
@@ -42,7 +55,9 @@ internal fun LazyGridScope.itemPhotosBlock(modifier: Modifier = Modifier) {
 private fun ItemPhotosBlock(modifier: Modifier = Modifier) {
     val context = LocalContext.current
     val controller = LocalController.current
+    val density = LocalDensity.current
     val theme = LocalTheme.current
+
     val activeSKUItem = controller.activeSKUItem.value
 
     val baseSharedModifier =
@@ -51,6 +66,11 @@ private fun ItemPhotosBlock(modifier: Modifier = Modifier) {
             .width(170.dp)
             .clip(theme.shapes.previewImage)
             .background(theme.colors.neutral)
+
+    val sharedRadius =
+        with(density) {
+            theme.shapes.previewImage.topStart.toPx(Size.Unspecified, density).toDp()
+        }
 
     LazyRow(
         modifier = modifier,
@@ -63,6 +83,9 @@ private fun ItemPhotosBlock(modifier: Modifier = Modifier) {
             key = { _, item -> item },
             contentType = { _, _ -> "ITEM_PHOTO_TYPE" },
         ) { index, url ->
+            var parentImageOffset by remember { mutableStateOf(Offset.Unspecified) }
+            var imageSize by remember { mutableStateOf(Size.Zero) }
+
             val finalModifier =
                 if (index == 0) {
                     baseSharedModifier.padding(24.dp)
@@ -78,7 +101,23 @@ private fun ItemPhotosBlock(modifier: Modifier = Modifier) {
                     modifier =
                         Modifier
                             .fillMaxSize()
-                            .clip(theme.shapes.previewImage),
+                            .clip(theme.shapes.previewImage)
+                            .onGloballyPositioned { coordinates ->
+                                parentImageOffset = coordinates.positionInRoot()
+                                imageSize = coordinates.size.toSize()
+                            }
+                            .clickableUnindicated {
+                                controller.zoomImageController.openZoomImageScreen(
+                                    model =
+                                        ZoomImageUiModel(
+                                            imageSize = imageSize,
+                                            initialCornerRadius = sharedRadius,
+                                            imageUrl = url,
+                                            parentImageOffset = parentImageOffset,
+                                            additionalShareInfo = controller.activeSKUItem.value.additionalShareInfo,
+                                        ),
+                                )
+                            },
                     model =
                         ImageRequest.Builder(context)
                             .data(url)
