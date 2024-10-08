38db8b977865a2113b2d32082c02d71b9ccd91bc
MishkaV <mavorozhtsov_1@edu.hse.ru>
2024-10-08 19:03:51 +0200

    Delete deprecated discount block

--- fashion-tryon-compose/src/main/kotlin/com/aiuta/fashionsdk/tryon/compose/ui/internal/components/block/SKUInfo.kt
+++ fashion-tryon-compose/src/main/kotlin/com/aiuta/fashionsdk/tryon/compose/ui/internal/components/block/SKUInfo.kt
@@ -1,19 +1,14 @@
 package com.aiuta.fashionsdk.tryon.compose.ui.internal.components.block
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.aiuta.fashionsdk.compose.tokens.composition.LocalTheme
 import com.aiuta.fashionsdk.tryon.compose.domain.models.SKUItem
@@ -49,37 +44,6 @@ internal fun SKUInfo(
         PriceInfo(
             modifier = Modifier.fillMaxWidth(),
             skuItem = skuItem,
-        )
-    }
-}
-
-@Deprecated("Need to delete")
-@Composable
-internal fun DiscountBlock(
-    modifier: Modifier = Modifier,
-    localizedDiscount: String,
-) {
-    val theme = LocalTheme.current
-
-    Box(
-        modifier =
-            modifier
-                .background(
-                    shape = RoundedCornerShape(4.dp),
-                    color = theme.colors.accent,
-                )
-                .padding(
-                    vertical = 2.dp,
-                    horizontal = 4.dp,
-                ),
-        contentAlignment = Alignment.Center,
-    ) {
-        Text(
-            text = "$localizedDiscount%",
-            style = theme.typography.price,
-            color = theme.colors.onDark,
-            maxLines = 1,
-            overflow = TextOverflow.Ellipsis,
         )
     }
 }
