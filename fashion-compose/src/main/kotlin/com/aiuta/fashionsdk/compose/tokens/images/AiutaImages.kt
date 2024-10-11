1d1ac714cd48c7548eca64fbb3ee847920fb19ac
MishkaV <mavorozhtsov_1@edu.hse.ru>
2024-10-11 15:31:16 +0200

    Make preonboarding image nullable

--- fashion-compose/src/main/kotlin/com/aiuta/fashionsdk/compose/tokens/images/AiutaImages.kt
+++ fashion-compose/src/main/kotlin/com/aiuta/fashionsdk/compose/tokens/images/AiutaImages.kt
@@ -5,5 +5,5 @@ import androidx.compose.runtime.Immutable
 // TODO Add docs
 @Immutable
 public class AiutaImages(
-    public val preonboardingImage: AiutaImage,
+    public val preonboardingImage: AiutaImage?,
 )
