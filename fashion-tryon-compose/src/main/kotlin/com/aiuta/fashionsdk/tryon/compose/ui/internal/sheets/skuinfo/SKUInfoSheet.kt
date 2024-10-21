b4f393605124212d28cc66b690494e42bbe717fa
MishkaV <mavorozhtsov_1@edu.hse.ru>
2024-10-21 23:11:36 +0200

    Change button add to wish in sheet

--- fashion-tryon-compose/src/main/kotlin/com/aiuta/fashionsdk/tryon/compose/ui/internal/sheets/skuinfo/SKUInfoSheet.kt
+++ fashion-tryon-compose/src/main/kotlin/com/aiuta/fashionsdk/tryon/compose/ui/internal/sheets/skuinfo/SKUInfoSheet.kt
@@ -142,9 +142,14 @@ private fun ButtonsContainer(
                     Modifier
                         .weight(1f)
                         .fillMaxHeight(),
+                icon = if (activeSKUItem.inWishlist) {
+                    theme.icons.wishlistFill24
+                } else {
+                    theme.icons.wishlist24
+                },
                 text = stringResources.addToWish,
                 style = FashionButtonStyles.secondaryStyle(theme),
-                size = FashionButtonSizes.lSize(),
+                size = FashionButtonSizes.lSize(iconSize = 20.dp),
                 onClick = {
                     controller.clickAddToWishListActiveSKU(skuId = activeSKUItem.skuId)
                 },
