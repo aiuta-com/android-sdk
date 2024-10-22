7c9b7c49cd664313dd10c57690375d858140a4dd
MishkaV <mavorozhtsov_1@edu.hse.ru>
2024-10-22 13:40:37 +0200

    Change color of placeholder

--- fashion-tryon-compose/src/main/kotlin/com/aiuta/fashionsdk/tryon/compose/ui/internal/screens/selector/components/body/Images.kt
+++ fashion-tryon-compose/src/main/kotlin/com/aiuta/fashionsdk/tryon/compose/ui/internal/screens/selector/components/body/Images.kt
@@ -1,19 +1,21 @@
 package com.aiuta.fashionsdk.tryon.compose.ui.internal.screens.selector.components.body
 
-import androidx.compose.foundation.Image
+import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.vectorResource
+import com.aiuta.fashionsdk.compose.tokens.composition.LocalTheme
 import com.aiuta.fashionsdk.tryon.compose.R
 
 @Composable
 internal fun DefaultImage(modifier: Modifier = Modifier) {
-    Image(
+    val theme = LocalTheme.current
+
+    Icon(
         modifier = modifier,
         imageVector = ImageVector.vectorResource(R.drawable.demo_image_selector),
         contentDescription = null,
-        contentScale = ContentScale.Fit,
+        tint = theme.colors.neutral3,
     )
 }
