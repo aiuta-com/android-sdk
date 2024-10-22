64810b6babad4aab24cf5fe3e251a06dc8d97474
MishkaV <mavorozhtsov_1@edu.hse.ru>
2024-10-22 16:05:33 +0200

    Refactor result screen

--- fashion-tryon-compose/src/main/kotlin/com/aiuta/fashionsdk/tryon/compose/ui/internal/screens/result/GenerationResultScreen.kt
+++ fashion-tryon-compose/src/main/kotlin/com/aiuta/fashionsdk/tryon/compose/ui/internal/screens/result/GenerationResultScreen.kt
@@ -1,25 +1,31 @@
 package com.aiuta.fashionsdk.tryon.compose.ui.internal.screens.result
 
-import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.BottomSheetScaffold
+import androidx.compose.material.BottomSheetValue
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.lerp
 import androidx.compose.ui.unit.dp
+import com.aiuta.fashionsdk.compose.tokens.composition.LocalTheme
 import com.aiuta.fashionsdk.internal.analytic.model.AiutaAnalyticPageId
 import com.aiuta.fashionsdk.tryon.compose.ui.internal.analytic.sendPageEvent
 import com.aiuta.fashionsdk.tryon.compose.ui.internal.components.appbar.MainAppBar
 import com.aiuta.fashionsdk.tryon.compose.ui.internal.screens.result.components.body.GenerationResultBody
 import com.aiuta.fashionsdk.tryon.compose.ui.internal.screens.result.components.common.ThanksFeedbackBlock
 import com.aiuta.fashionsdk.tryon.compose.ui.internal.screens.result.components.footer.DisclaimerBlock
-import com.aiuta.fashionsdk.tryon.compose.ui.internal.screens.result.components.footer.GenerationResultFooter
+import com.aiuta.fashionsdk.tryon.compose.ui.internal.screens.result.components.footer.GenerationResultFooterList
+import com.aiuta.fashionsdk.tryon.compose.ui.internal.screens.result.controller.GenerationResultController
 import com.aiuta.fashionsdk.tryon.compose.ui.internal.screens.result.controller.GenerationResultListener
-import com.aiuta.fashionsdk.tryon.compose.ui.internal.screens.result.controller.appbarHeight
-import com.aiuta.fashionsdk.tryon.compose.ui.internal.screens.result.controller.disclaimerHeight
-import com.aiuta.fashionsdk.tryon.compose.ui.internal.screens.result.controller.disclaimerOffset
-import com.aiuta.fashionsdk.tryon.compose.ui.internal.screens.result.controller.imagesHeight
 import com.aiuta.fashionsdk.tryon.compose.ui.internal.screens.result.controller.rememberGenerationResultController
 
 @Composable
@@ -33,47 +39,106 @@ internal fun GenerationResultScreen(modifier: Modifier = Modifier) {
 
 @Composable
 private fun GenerationResultScreenContent(modifier: Modifier = Modifier) {
-    BoxWithConstraints(
+    val theme = LocalTheme.current
+    val generationResultController = rememberGenerationResultController()
+
+    BottomSheetScaffold(
+        modifier = modifier,
+        scaffoldState = generationResultController.bottomSheetScaffoldState,
+        sheetContent = {
+            GenerationResultFooterList(
+                modifier = Modifier.fillMaxSize(),
+                generationResultController = generationResultController,
+            )
+        },
+        sheetBackgroundColor = theme.colors.background,
+        backgroundColor = theme.colors.neutral,
+        sheetShape = theme.shapes.bottomSheet,
+        sheetPeekHeight = 180.dp,
+    ) { paddings ->
+        Box(
+            modifier =
+                Modifier
+                    .padding(paddings)
+                    .background(theme.colors.background),
+        ) {
+            BottomSheetScaffoldContent(
+                modifier = Modifier.fillMaxSize(),
+                generationResultController = generationResultController,
+            )
+
+            ThanksFeedbackBlock(
+                modifier = Modifier.align(Alignment.Center),
+                generationResultController = generationResultController,
+            )
+
+            BottomSheetScaffoldScrim(
+                modifier = Modifier.fillMaxSize(),
+                generationResultController = generationResultController,
+            )
+        }
+    }
+}
+
+@Composable
+private fun BottomSheetScaffoldContent(
+    modifier: Modifier = Modifier,
+    generationResultController: GenerationResultController,
+) {
+    Column(
         modifier = modifier,
     ) {
-        val generationResultController = rememberGenerationResultController(maxHeight = maxHeight)
-
         MainAppBar(
             modifier =
                 Modifier
-                    .align(Alignment.TopCenter)
-                    .height(appbarHeight(maxHeight))
                     .fillMaxWidth()
                     .padding(horizontal = 16.dp),
         )
 
+        Spacer(Modifier.height(16.dp))
+
         GenerationResultBody(
             modifier =
                 Modifier
-                    .align(Alignment.TopCenter)
-                    .padding(top = appbarHeight(maxHeight))
-                    .height(imagesHeight(maxHeight))
                     .fillMaxWidth()
-                    .padding(vertical = 16.dp),
+                    .weight(1f),
             generationResultController = generationResultController,
         )
+
+        Spacer(Modifier.height(32.dp))
 
         DisclaimerBlock(
             modifier =
                 Modifier
-                    .align(Alignment.TopCenter)
-                    .padding(top = disclaimerOffset(maxHeight))
-                    .height(disclaimerHeight())
+                    .height(34.dp)
                     .fillMaxWidth(),
         )
-
-        GenerationResultFooter(
-            generationResultController = generationResultController,
-        )
-
-        ThanksFeedbackBlock(
-            modifier = Modifier.align(Alignment.Center),
-            generationResultController = generationResultController,
-        )
     }
+}
+
+@Composable
+private fun BottomSheetScaffoldScrim(
+    modifier: Modifier = Modifier,
+    generationResultController: GenerationResultController,
+) {
+    val theme = LocalTheme.current
+
+    val sheetProgress =
+        generationResultController
+            .bottomSheetScaffoldState
+            .bottomSheetState
+            .progress(
+                from = BottomSheetValue.Collapsed,
+                to = BottomSheetValue.Expanded,
+            )
+    val scrimColor =
+        lerp(
+            start = Color.Transparent,
+            stop = theme.colors.primary.copy(alpha = 0.6f),
+            fraction = sheetProgress,
+        )
+
+    Box(
+        modifier = modifier.background(scrimColor),
+    )
 }
--- fashion-tryon-compose/src/main/kotlin/com/aiuta/fashionsdk/tryon/compose/ui/internal/screens/result/components/footer/GenerationResultFooter.kt
+++ fashion-tryon-compose/src/main/kotlin/com/aiuta/fashionsdk/tryon/compose/ui/internal/screens/result/components/footer/GenerationResultFooter.kt
@@ -1,29 +1,15 @@
 package com.aiuta.fashionsdk.tryon.compose.ui.internal.screens.result.components.footer
 
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.anchoredDraggable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraintsScope
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.navigationBars
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
-import com.aiuta.fashionsdk.compose.tokens.composition.LocalTheme
 import com.aiuta.fashionsdk.tryon.compose.ui.internal.controller.composition.LocalController
 import com.aiuta.fashionsdk.tryon.compose.ui.internal.screens.result.components.footer.blocks.common.dividerBlock
 import com.aiuta.fashionsdk.tryon.compose.ui.internal.screens.result.components.footer.blocks.common.spacerBlock
@@ -31,62 +17,61 @@ import com.aiuta.fashionsdk.tryon.compose.ui.internal.screens.result.components.
 import com.aiuta.fashionsdk.tryon.compose.ui.internal.screens.result.components.footer.blocks.more.generateMoreListBlock
 import com.aiuta.fashionsdk.tryon.compose.ui.internal.screens.result.components.footer.blocks.more.generateMoreTitleBlock
 import com.aiuta.fashionsdk.tryon.compose.ui.internal.screens.result.components.footer.blocks.photos.itemPhotosBlock
-import com.aiuta.fashionsdk.tryon.compose.ui.internal.screens.result.connection.rememberGenerationResultConnection
 import com.aiuta.fashionsdk.tryon.compose.ui.internal.screens.result.controller.GenerationResultController
-import kotlin.math.roundToInt
 
 internal const val FOOTER_FULL_SIZE_SPAN = 2
 
-@OptIn(ExperimentalFoundationApi::class)
+// TODO DElete
+// @OptIn(ExperimentalFoundationApi::class)
+// @Composable
+// internal fun BoxWithConstraintsScope.GenerationResultFooter(
+//    modifier: Modifier = Modifier,
+//    generationResultController: GenerationResultController,
+// ) {
+//    val theme = LocalTheme.current
+//
+//    val connection =
+//        rememberGenerationResultConnection(
+//            verticalSwipeState = generationResultController.verticalSwipeState,
+//        )
+//
+//    Box(
+//        modifier =
+//            modifier
+//                .fillMaxWidth()
+//                .offset {
+//                    IntOffset(
+//                        0,
+//                        (generationResultController.verticalSwipeState.offset).roundToInt(),
+//                    )
+//                }
+//                .background(
+//                    color = theme.colors.background,
+//                    shape = theme.shapes.bottomSheet,
+//                )
+//                .anchoredDraggable(
+//                    state = generationResultController.verticalSwipeState,
+//                    orientation = Orientation.Vertical,
+//                )
+//                .nestedScroll(connection),
+//    ) {
+//        GenerationResultFooterList(
+//            modifier =
+//                Modifier
+//                    .fillMaxWidth()
+//                    .wrapContentHeight()
+//                    .windowInsetsPadding(WindowInsets.navigationBars)
+//                    .onGloballyPositioned { coordination ->
+//                        generationResultController.footerHeightPx.floatValue =
+//                            coordination.size.height.toFloat()
+//                    },
+//            generationResultController = generationResultController,
+//        )
+//    }
+// }
+
 @Composable
-internal fun BoxWithConstraintsScope.GenerationResultFooter(
-    modifier: Modifier = Modifier,
-    generationResultController: GenerationResultController,
-) {
-    val theme = LocalTheme.current
-
-    val connection =
-        rememberGenerationResultConnection(
-            verticalSwipeState = generationResultController.verticalSwipeState,
-        )
-
-    Box(
-        modifier =
-            modifier
-                .fillMaxWidth()
-                .offset {
-                    IntOffset(
-                        0,
-                        (generationResultController.verticalSwipeState.offset).roundToInt(),
-                    )
-                }
-                .background(
-                    color = theme.colors.background,
-                    shape = theme.shapes.bottomSheet,
-                )
-                .anchoredDraggable(
-                    state = generationResultController.verticalSwipeState,
-                    orientation = Orientation.Vertical,
-                )
-                .nestedScroll(connection),
-    ) {
-        GenerationResultFooterList(
-            modifier =
-                Modifier
-                    .fillMaxWidth()
-                    .wrapContentHeight()
-                    .windowInsetsPadding(WindowInsets.navigationBars)
-                    .onGloballyPositioned { coordination ->
-                        generationResultController.footerHeightPx.floatValue =
-                            coordination.size.height.toFloat()
-                    },
-            generationResultController = generationResultController,
-        )
-    }
-}
-
-@Composable
-private fun GenerationResultFooterList(
+internal fun GenerationResultFooterList(
     modifier: Modifier = Modifier,
     generationResultController: GenerationResultController,
 ) {
