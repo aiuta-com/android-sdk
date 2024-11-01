a324dd2f85bc9f6f83bb5b0b5760ea8b6717d806
MishkaV <mavorozhtsov_1@edu.hse.ru>
2024-11-01 10:07:35 +0100

    Delete not used handlers

--- fashion-tryon-core/src/main/kotlin/com/aiuta/fashionsdk/tryon/core/data/datasource/sku/FashionSKUDataSource.kt
+++ fashion-tryon-core/src/main/kotlin/com/aiuta/fashionsdk/tryon/core/data/datasource/sku/FashionSKUDataSource.kt
@@ -2,7 +2,6 @@ package com.aiuta.fashionsdk.tryon.core.data.datasource.sku
 
 import com.aiuta.fashionsdk.network.paging.models.PageContainer
 import com.aiuta.fashionsdk.network.paging.models.PaginationOffset
-import com.aiuta.fashionsdk.tryon.core.data.datasource.sku.models.CreateSKUItemRequest
 import com.aiuta.fashionsdk.tryon.core.data.datasource.sku.models.SKUCatalogDTO
 import com.aiuta.fashionsdk.tryon.core.data.datasource.sku.models.SKUItemDTO
 
@@ -13,12 +12,6 @@ internal interface FashionSKUDataSource {
         paginationLimit: Int? = null,
     ): PageContainer<SKUCatalogDTO>
 
-    // SKU items
-    suspend fun createSKUItem(
-        skuCatalogName: String,
-        request: CreateSKUItemRequest,
-    ): SKUItemDTO
-
     suspend fun getSKUItems(
         skuCatalogName: String,
         paginationOffset: PaginationOffset? = null,
@@ -26,11 +19,6 @@ internal interface FashionSKUDataSource {
     ): PageContainer<SKUItemDTO>
 
     suspend fun getSKUItem(
-        skuCatalogName: String,
-        skuId: String,
-    ): SKUItemDTO
-
-    suspend fun deleteSKUItem(
         skuCatalogName: String,
         skuId: String,
     ): SKUItemDTO
--- fashion-tryon-core/src/main/kotlin/com/aiuta/fashionsdk/tryon/core/data/datasource/sku/internal/FashionSKURemoteDataSource.kt
+++ fashion-tryon-core/src/main/kotlin/com/aiuta/fashionsdk/tryon/core/data/datasource/sku/internal/FashionSKURemoteDataSource.kt
@@ -6,14 +6,10 @@ import com.aiuta.fashionsdk.network.paging.models.PaginationOffset
 import com.aiuta.fashionsdk.network.paging.utils.saveAppend
 import com.aiuta.fashionsdk.network.utils.saveAppendLimit
 import com.aiuta.fashionsdk.tryon.core.data.datasource.sku.FashionSKUDataSource
-import com.aiuta.fashionsdk.tryon.core.data.datasource.sku.models.CreateSKUItemRequest
 import com.aiuta.fashionsdk.tryon.core.data.datasource.sku.models.SKUCatalogDTO
 import com.aiuta.fashionsdk.tryon.core.data.datasource.sku.models.SKUItemDTO
 import io.ktor.client.call.body
-import io.ktor.client.request.delete
 import io.ktor.client.request.get
-import io.ktor.client.request.post
-import io.ktor.client.request.setBody
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
@@ -33,20 +29,6 @@ internal class FashionSKURemoteDataSource(
                     saveAppend(paginationOffset)
                     saveAppendLimit(paginationLimit)
                 }
-            }.body()
-        }
-    }
-
-    // SKU items
-    override suspend fun createSKUItem(
-        skuCatalogName: String,
-        request: CreateSKUItemRequest,
-    ): SKUItemDTO {
-        return withContext(Dispatchers.IO) {
-            networkClient.httpClient.value.post(
-                urlString = "$PATH_SKU_ITEMS/$skuCatalogName",
-            ) {
-                setBody(request)
             }.body()
         }
     }
@@ -74,17 +56,6 @@ internal class FashionSKURemoteDataSource(
     ): SKUItemDTO {
         return withContext(Dispatchers.IO) {
             networkClient.httpClient.value.get(
-                urlString = "$PATH_SKU_ITEMS/$skuCatalogName/$skuId",
-            ).body()
-        }
-    }
-
-    override suspend fun deleteSKUItem(
-        skuCatalogName: String,
-        skuId: String,
-    ): SKUItemDTO {
-        return withContext(Dispatchers.IO) {
-            networkClient.httpClient.value.delete(
                 urlString = "$PATH_SKU_ITEMS/$skuCatalogName/$skuId",
             ).body()
         }
