b1847704cc61c72b608bf65180fe744ff9b20094
Mishka Vorozhtsov <54765046+MishkaV@users.noreply.github.com>
2024-11-19 14:00:45 +0100

    Add supplementary consents to analytic (#234)
    * Add supplementary consents to analytic
    
    * Update apu dump

--- fashion-tryon-compose/src/main/kotlin/com/aiuta/fashionsdk/tryon/compose/domain/models/dataprovider/SupplementaryConsent.kt
+++ fashion-tryon-compose/src/main/kotlin/com/aiuta/fashionsdk/tryon/compose/domain/models/dataprovider/SupplementaryConsent.kt
@@ -1,6 +1,15 @@
 package com.aiuta.fashionsdk.tryon.compose.domain.models.dataprovider
 
+import com.aiuta.fashionsdk.internal.analytic.model.AiutaSupplementaryConsent
+
 public class SupplementaryConsent(
     public val consentText: String,
     public val isObtained: Boolean,
 )
+
+internal fun SupplementaryConsent.toAnalytic(): AiutaSupplementaryConsent {
+    return AiutaSupplementaryConsent(
+        consentText = consentText,
+        isObtained = isObtained,
+    )
+}
