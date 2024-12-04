338cf7e91034b9bfe11a82908e3f8a4f93407674
Mishka Vorozhtsov <54765046+MishkaV@users.noreply.github.com>
2024-12-04 16:26:55 +0100

    Change align for checkboxes on consent page (#245)

--- fashion-tryon-compose/src/main/kotlin/com/aiuta/fashionsdk/tryon/compose/ui/internal/screens/onboarding/components/consent/ConsentPageContent.kt
+++ fashion-tryon-compose/src/main/kotlin/com/aiuta/fashionsdk/tryon/compose/ui/internal/screens/onboarding/components/consent/ConsentPageContent.kt
@@ -17,9 +17,12 @@ import androidx.compose.material.LocalRippleConfiguration
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.aiuta.fashionsdk.compose.tokens.composition.LocalTheme
@@ -31,7 +34,9 @@ import com.aiuta.fashionsdk.tryon.compose.ui.internal.controller.composition.Loc
 import com.aiuta.fashionsdk.tryon.compose.ui.internal.screens.onboarding.controller.OnboardingController
 import com.aiuta.fashionsdk.tryon.compose.ui.internal.screens.onboarding.controller.updateMandatoryAgreementState
 import com.aiuta.fashionsdk.tryon.compose.ui.internal.screens.onboarding.controller.updateSupplementAgreementState
+import com.aiuta.fashionsdk.tryon.compose.ui.internal.utils.CenterAlignmentLine
 import com.aiuta.fashionsdk.tryon.compose.ui.internal.utils.buildAnnotatedStringFromHtml
+import com.aiuta.fashionsdk.tryon.compose.ui.internal.utils.createCenterAlignmentLine
 
 @Composable
 internal fun ConsentPageContent(
@@ -149,13 +154,20 @@ private fun AgreePoint(
 ) {
     val theme = LocalTheme.current
 
+    val lineIndexToCenter = 0
+    var topTextPosition by remember { mutableFloatStateOf(0f) }
+    var bottomTextPosition by remember { mutableFloatStateOf(0f) }
+
     Row(
         modifier = modifier,
-        verticalAlignment = Alignment.CenterVertically,
     ) {
         CompositionLocalProvider(LocalRippleConfiguration provides null) {
             Checkbox(
-                modifier = Modifier.size(20.dp),
+                modifier =
+                    Modifier
+                        .size(20.dp)
+                        .alignBy(CenterAlignmentLine)
+                        .createCenterAlignmentLine(),
                 checked = isAgreementChecked,
                 onCheckedChange = onAgreementCheckedChange,
                 colors =
@@ -175,11 +187,17 @@ private fun AgreePoint(
                     .weight(1f)
                     .clickableUnindicated {
                         onAgreementCheckedChange(!isAgreementChecked)
-                    },
+                    }
+                    .alignBy(CenterAlignmentLine)
+                    .createCenterAlignmentLine(topTextPosition, bottomTextPosition),
             text = text,
             style = theme.typography.regular,
             color = theme.colors.primary,
             textAlign = TextAlign.Start,
+            onTextLayout = { textLayout: TextLayoutResult ->
+                topTextPosition = textLayout.getLineTop(lineIndexToCenter)
+                bottomTextPosition = textLayout.getLineBottom(lineIndexToCenter)
+            },
         )
     }
 }
