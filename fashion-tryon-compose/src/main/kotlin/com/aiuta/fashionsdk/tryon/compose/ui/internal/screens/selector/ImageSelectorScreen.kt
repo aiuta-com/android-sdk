d8651520845e303c1f92801819f96d381b56f6f8
MishkaV <mavorozhtsov_1@edu.hse.ru>
2025-03-13 15:10:58 +0500

    Delete comments

--- fashion-tryon-compose/src/main/kotlin/com/aiuta/fashionsdk/tryon/compose/ui/internal/screens/selector/ImageSelectorScreen.kt
+++ fashion-tryon-compose/src/main/kotlin/com/aiuta/fashionsdk/tryon/compose/ui/internal/screens/selector/ImageSelectorScreen.kt
@@ -15,58 +15,9 @@ import com.aiuta.fashionsdk.tryon.compose.ui.internal.screens.selector.controlle
 
 @Composable
 internal fun ImageSelectorScreen(modifier: Modifier = Modifier) {
-    val controller = LocalController.current
-    val theme = LocalTheme.current
-
-    val generationStatus = controller.generationStatus
-
-    val isLastSavedPhotoAvailable = controller.isLastSavedPhotoAvailable()
-    val isTryOnButtonVisible =
-        remember {
-            derivedStateOf {
-                generationStatus.value != SKUGenerationUIStatus.LOADING && isLastSavedPhotoAvailable.value
-            }
-        }
-
     sendPageEvent(pageId = AiutaAnalyticPageId.IMAGE_PICKER)
 
     ImageSelectorAutoTryOnListener()
 
-    ImageSelectorScreenContent(
-        modifier = modifier,
-    )
-
-//    Column(
-//        modifier = modifier.background(theme.colors.background),
-//        horizontalAlignment = Alignment.CenterHorizontally,
-//    ) {
-//        MainAppBar(
-//            modifier =
-//                Modifier
-//                    .fillMaxWidth()
-//                    .padding(horizontal = 16.dp),
-//        )
-//
-//        Spacer(Modifier.height(16.dp))
-//
-//        ImageSelectorBlock(
-//            modifier = Modifier.fillMaxSize(MAIN_IMAGE_SIZE),
-//            uploadPhoto = {
-//                controller.bottomSheetNavigator.show(
-//                    newSheetScreen =
-//                        NavigationBottomSheetScreen.ImagePicker(
-//                            originPageId = AiutaAnalyticPageId.IMAGE_PICKER,
-//                        ),
-//                )
-//            },
-//        )
-//
-//        ImageSelectorFooter(
-//            modifier =
-//                Modifier
-//                    .fillMaxWidth()
-//                    .fillMaxHeight(),
-//            isTryOnButtonVisible = isTryOnButtonVisible,
-//        )
-//    }
+    ImageSelectorScreenContent(modifier = modifier)
 }
