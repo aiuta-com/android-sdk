40d6e6be7be325a967f7a6d1882797e9da6f42e3
MishkaV <mavorozhtsov_1@edu.hse.ru>
2025-03-19 14:50:19 +0300

    Fix lint

--- fashion-tryon-compose/src/main/kotlin/com/aiuta/fashionsdk/tryon/compose/ui/internal/utils/Settings.kt
+++ fashion-tryon-compose/src/main/kotlin/com/aiuta/fashionsdk/tryon/compose/ui/internal/utils/Settings.kt
@@ -4,10 +4,11 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+import androidx.core.net.toUri
 
 internal fun Context.openSettings() {
     try {
-        Intent(ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:$packageName")).apply {
+        Intent(ACTION_APPLICATION_DETAILS_SETTINGS, "package:$packageName".toUri()).apply {
             addCategory(Intent.CATEGORY_DEFAULT)
             addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
             startActivity(this)
