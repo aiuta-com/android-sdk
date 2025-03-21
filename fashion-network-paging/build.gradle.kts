2c603d37ea53e422476cbdfcb00b1d5e09c4e4cc
MishkaV <mavorozhtsov_1@edu.hse.ru>
2025-03-21 12:54:50 +0300

    Migrate fashion-network-paging to KMP

--- fashion-network-paging/build.gradle.kts
+++ fashion-network-paging/build.gradle.kts
@@ -1,18 +1,25 @@
-import com.aiuta.fashionsdk.androidLibrary
+import com.aiuta.fashionsdk.addAllMultiplatformTargets
+import com.aiuta.fashionsdk.androidLibraryV2
 
 plugins {
     id("com.android.library")
-    id("kotlin-android")
+    id("kotlin-multiplatform")
     alias(libs.plugins.kotlinx.serialization)
 }
 
-androidLibrary(name = "com.aiuta.fashionsdk.network.paging")
+addAllMultiplatformTargets()
+androidLibraryV2(name = "com.aiuta.fashionsdk.network.paging")
 
-dependencies {
-    api(projects.fashionNetwork)
+kotlin {
+    sourceSets {
+        commonMain {
+            dependencies {
+                api(projects.fashionNetwork)
 
-    implementation(libs.androidx.paging)
-    implementation(libs.kotlinx.coroutines.core)
-    implementation(libs.kotlinx.serialization)
-    implementation(libs.ktor.core)
+                implementation(libs.androidx.paging.common)
+                implementation(libs.kotlinx.serialization)
+                implementation(libs.ktor.core)
+            }
+        }
+    }
 }
