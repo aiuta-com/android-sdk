ddf891e1ed38df9939245a6beaa07db1dbad26aa
MishkaV <mavorozhtsov_1@edu.hse.ru>
2025-03-22 14:36:40 +0300

    Update api dump

--- fashion-tryon-core/src/commonMain/kotlin/com/aiuta/fashionsdk/tryon/core/domain/models/image/PlatformImage.kt
+++ fashion-tryon-core/src/commonMain/kotlin/com/aiuta/fashionsdk/tryon/core/domain/models/image/PlatformImage.kt
@@ -3,7 +3,7 @@ package com.aiuta.fashionsdk.tryon.core.domain.models.image
 import androidx.compose.ui.graphics.ImageBitmap
 
 public expect class PlatformImage {
-    val byteArray: ByteArray
+    public val byteArray: ByteArray
 
-    val imageBitmap: ImageBitmap
+    public val imageBitmap: ImageBitmap
 }
