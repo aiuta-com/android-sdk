dd7730a87a9cad7b23597754e63479f0dd4e7b16
MishkaV <mavorozhtsov_1@edu.hse.ru>
2025-04-09 15:38:22 +0200

    Make more smooth change of aiuta image

--- fashion-tryon-compose-uikit/src/commonMain/kotlin/com/aiuta/fashionsdk/tryon/compose/uikit/resources/AiutaImage.kt
+++ fashion-tryon-compose-uikit/src/commonMain/kotlin/com/aiuta/fashionsdk/tryon/compose/uikit/resources/AiutaImage.kt
@@ -1,6 +1,8 @@
 package com.aiuta.fashionsdk.tryon.compose.uikit.resources
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.shape.CornerBasedShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
@@ -93,22 +95,27 @@ public fun AiutaImage(
         derivedStateOf { imageModelState.value is AsyncImagePainter.State.Loading }
     }
 
-    when (imageModelState.value) {
-        is AsyncImagePainter.State.Empty -> Unit
-        is AsyncImagePainter.State.Error -> ErrorProgress(modifier = modifier)
-        else -> {
-            Image(
-                modifier = modifier.placeholderFadeConnecting(
-                    shape = shape,
-                    visible = isShimmerVisible.value,
-                ),
-                painter = imagePainter,
-                contentScale = contentScale,
-                alignment = alignment,
-                alpha = alpha,
-                colorFilter = colorFilter,
-                contentDescription = contentDescription,
-            )
+    // Need box for more smooth transition
+    Box(modifier = modifier) {
+        when (imageModelState.value) {
+            is AsyncImagePainter.State.Empty -> Unit
+            is AsyncImagePainter.State.Error -> ErrorProgress(modifier = modifier)
+            else -> {
+                Image(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .placeholderFadeConnecting(
+                            shape = shape,
+                            visible = isShimmerVisible.value,
+                        ),
+                    painter = imagePainter,
+                    contentScale = contentScale,
+                    alignment = alignment,
+                    alpha = alpha,
+                    colorFilter = colorFilter,
+                    contentDescription = contentDescription,
+                )
+            }
         }
     }
 }
