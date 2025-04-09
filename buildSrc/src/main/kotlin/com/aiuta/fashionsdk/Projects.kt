0d47954bb3673e9f9494fc4b5c109064ef24a6dc
MishkaV <mavorozhtsov_1@edu.hse.ru>
2025-04-09 22:23:32 +0200

    Delete deprecated modules and buildSrc extensions

--- buildSrc/src/main/kotlin/com/aiuta/fashionsdk/Projects.kt
+++ buildSrc/src/main/kotlin/com/aiuta/fashionsdk/Projects.kt
@@ -4,58 +4,21 @@ import com.android.build.api.dsl.CommonExtension
 import com.android.build.api.dsl.Lint
 import com.android.build.gradle.BaseExtension
 import com.android.build.gradle.LibraryExtension
-import com.android.build.gradle.TestExtension
 import com.android.build.gradle.internal.dsl.BaseAppModuleExtension
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
-import org.gradle.kotlin.dsl.assign
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.get
 import org.gradle.kotlin.dsl.withType
-import org.jetbrains.kotlin.compose.compiler.gradle.ComposeCompilerGradlePluginExtension
 import org.jetbrains.kotlin.gradle.dsl.ExplicitApiMode
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-@Deprecated("Use androidLibraryV2 instead")
 fun Project.androidLibrary(
     name: String,
     config: Boolean = false,
-    composeLibrary: Boolean = false,
     action: LibraryExtension.() -> Unit = {},
 ) = androidBase<LibraryExtension>(
-    name = name,
-    config = config,
-) {
-    buildFeatures {
-        buildConfig = config
-    }
-    if (project.name in publicModules) {
-        apply(plugin = "org.jetbrains.dokka")
-        setupAndroidPublishing<LibraryExtension>()
-    }
-    if (composeLibrary) {
-        apply(plugin = "org.jetbrains.kotlin.plugin.compose")
-        extensions.configure<ComposeCompilerGradlePluginExtension> {
-            enableStrongSkippingMode = true
-        }
-        buildFeatures {
-            compose = true
-        }
-    }
-    if (config) {
-        defaultConfig {
-            buildConfigField("String", "VERSION_NAME", "\"${versionName}\"")
-        }
-    }
-    action()
-}
-
-fun Project.androidLibraryV2(
-    name: String,
-    config: Boolean = false,
-    action: LibraryExtension.() -> Unit = {},
-) = androidBaseV2<LibraryExtension>(
     name = name,
     config = config,
 ) {
@@ -79,37 +42,10 @@ fun Project.androidLibraryV2(
 }
 
 
-@Deprecated("Use androidApplicationV2 instead")
 fun Project.androidApplication(
     name: String,
-    composeApp: Boolean = false,
     action: BaseAppModuleExtension.() -> Unit = {},
-) = androidBaseV2<BaseAppModuleExtension>(
-    name = name,
-) {
-    defaultConfig {
-        applicationId = name
-        versionCode = project.versionCode
-        versionName = project.versionName
-        resourceConfigurations += "en"
-        vectorDrawables.useSupportLibrary = true
-    }
-    buildFeatures {
-        compose = composeApp
-    }
-    if (composeApp) {
-        apply(plugin = "org.jetbrains.kotlin.plugin.compose")
-        extensions.configure<ComposeCompilerGradlePluginExtension> {
-            enableStrongSkippingMode = true
-        }
-    }
-    action()
-}
-
-fun Project.androidApplicationV2(
-    name: String,
-    action: BaseAppModuleExtension.() -> Unit = {},
-) = androidBaseV2<BaseAppModuleExtension>(
+) = androidBase<BaseAppModuleExtension>(
     name = name,
 ) {
     defaultConfig {
@@ -122,113 +58,7 @@ fun Project.androidApplicationV2(
     action()
 }
 
-
-@Deprecated("Use androidTestV2 instead")
-fun Project.androidTest(
-    name: String,
-    config: Boolean = false,
-    shouldBePublic: Boolean = false,
-    action: TestExtension.() -> Unit = {},
-) = androidBase<TestExtension>(
-    name = name,
-    config = config,
-    shouldBePublic = shouldBePublic,
-) {
-    buildFeatures {
-        buildConfig = config
-    }
-    defaultConfig {
-        resourceConfigurations += "en"
-        vectorDrawables.useSupportLibrary = true
-    }
-    action()
-}
-
-fun Project.androidTestV2(
-    name: String,
-    config: Boolean = false,
-    action: TestExtension.() -> Unit = {},
-) = androidBaseV2<TestExtension>(name) {
-    buildFeatures {
-        buildConfig = config
-    }
-    defaultConfig {
-        vectorDrawables.useSupportLibrary = true
-    }
-    action()
-}
-
-@Deprecated("Use androidBaseV2 instead")
 private fun <T : BaseExtension> Project.androidBase(
-    name: String,
-    config: Boolean = false,
-    shouldBePublic: Boolean = true,
-    action: T.() -> Unit,
-) {
-    android<T> {
-        namespace = name
-        compileSdkVersion(project.compileSdk)
-        defaultConfig {
-            minSdk = project.minSdk
-            targetSdk = project.targetSdk
-            testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
-            if (config) {
-                buildConfigField("String", "VERSION_NAME", "\"${versionName}\"")
-            }
-        }
-        packagingOptions {
-            resources.pickFirsts +=
-                listOf(
-                    "META-INF/AL2.0",
-                    "META-INF/LGPL2.1",
-                    "META-INF/*kotlin_module",
-                )
-        }
-        testOptions {
-            unitTests.isIncludeAndroidResources = true
-        }
-        lint {
-            warningsAsErrors = true
-            disable +=
-                listOf(
-                    "ComposableNaming",
-                    "UnknownIssueId",
-                    "VectorPath",
-                    "VectorRaster",
-                    "ObsoleteLintCustomCheck",
-                    "MonochromeLauncherIcon",
-                    "IconLocation",
-                )
-        }
-        action()
-    }
-    tasks.withType<KotlinCompile>().configureEach {
-        compilerOptions {
-            val arguments = mutableListOf<String>()
-
-            // https://kotlinlang.org/docs/compiler-reference.html#progressive
-            arguments += "-progressive"
-
-            // Enable Java default method generation.
-            arguments += "-Xjvm-default=all"
-
-            // Generate smaller bytecode by not generating runtime not-null assertions.
-            arguments += "-Xno-call-assertions"
-            arguments += "-Xno-param-assertions"
-            arguments += "-Xno-receiver-assertions"
-
-            // For explicitApi strict mode - make as error
-            if (shouldBePublic) {
-                arguments += "-Xexplicit-api=strict"
-            }
-
-            freeCompilerArgs.addAll(arguments)
-        }
-    }
-}
-
-private fun <T : BaseExtension> Project.androidBaseV2(
     name: String,
     config: Boolean = false,
     action: T.() -> Unit,
