6951363c9dc8569cf3bd299afb823024965393a1
MishkaV <mavorozhtsov_1@edu.hse.ru>
2025-04-14 19:13:44 +0200

    Add check of image picker resource

--- fashion-tryon-compose/src/jvmMain/kotlin/com/aiuta/fashionsdk/tryon/compose/ui/internal/utils/permission/PermissionUtils.jvm.kt
+++ fashion-tryon-compose/src/jvmMain/kotlin/com/aiuta/fashionsdk/tryon/compose/ui/internal/utils/permission/PermissionUtils.jvm.kt
@@ -2,3 +2,5 @@ package com.aiuta.fashionsdk.tryon.compose.ui.internal.utils.permission
 
 // For desktop we shouldn't ask permission
 internal actual fun shouldAskPermission(permission: AiutaPickerSource): Boolean = false
+
+internal actual fun isPickerSourceAvailable(permission: AiutaPickerSource): Boolean = false
