fcffeb754040784b1dec3dd1555d02f56d020e72
Mishka Vorozhtsov <54765046+MishkaV@users.noreply.github.com>
2025-04-22 10:38:30 +0200

    Add support of html on all platforms (#311)
    * Add support of html on all platforms
    
    * Update api dump

--- fashion-tryon-compose/build.gradle.kts
+++ fashion-tryon-compose/build.gradle.kts
@@ -51,6 +51,8 @@ kotlin {
                 implementation(libs.kotlinx.atomicfu)
                 implementation(libs.kotlinx.datetime)
                 implementation(libs.kotlinx.serialization)
+                implementation(libs.ksoup.html)
+                implementation(libs.ksoup.entities)
                 implementation(libs.room.runtime)
                 implementation(libs.room.paging)
                 implementation(libs.jetbrains.lifecycle)
