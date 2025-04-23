62c8c31e29bc35f478bd870b562f2700ad30dbfd
MishkaV <mavorozhtsov_1@edu.hse.ru>
2025-04-23 20:56:56 +0200

    Remove TODO

--- fashion-tryon-compose-defaults/src/commonMain/kotlin/com/aiuta/fashionsdk/tryon/compose/defaults/features/share/DefaultAiutaShareFeature.kt
+++ fashion-tryon-compose-defaults/src/commonMain/kotlin/com/aiuta/fashionsdk/tryon/compose/defaults/features/share/DefaultAiutaShareFeature.kt
@@ -3,20 +3,10 @@ package com.aiuta.fashionsdk.tryon.compose.defaults.features.share
 import com.aiuta.fashionsdk.tryon.compose.configuration.features.AiutaTryOnConfigurationFeatures
 import com.aiuta.fashionsdk.tryon.compose.configuration.features.share.share
 import com.aiuta.fashionsdk.tryon.compose.configuration.features.share.strings.AiutaShareFeatureStrings
-import com.aiuta.fashionsdk.tryon.compose.configuration.features.share.watermark.images.AiutaShareWatermarkFeatureImages
-import com.aiuta.fashionsdk.tryon.compose.configuration.features.share.watermark.watermark
 import com.aiuta.fashionsdk.tryon.compose.defaults.icons.features.share.DefaultAiutaShareFeatureIcons
-import com.aiuta.fashionsdk.tryon.compose.defaults.icons.features.welcome.DefaultAiutaWelcomeScreenFeatureIcons
-import com.aiuta.fashionsdk.tryon.compose.resources.drawable.AiutaDrawableResource
 
 public fun AiutaTryOnConfigurationFeatures.Builder.defaultShare() {
     share {
-        // TODO
-        watermark {
-            images = object : AiutaShareWatermarkFeatureImages {
-                override val logo: AiutaDrawableResource = DefaultAiutaWelcomeScreenFeatureIcons().welcome82.iconResource
-            }
-        }
         icons = DefaultAiutaShareFeatureIcons()
         strings = AiutaShareFeatureStrings.Default()
     }
