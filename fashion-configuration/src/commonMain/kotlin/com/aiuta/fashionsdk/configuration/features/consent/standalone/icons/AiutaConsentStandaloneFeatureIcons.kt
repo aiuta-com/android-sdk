61279b139a16350a4d50d7fca87343ea46804fc5
Mishka Vorozhtsov <54765046+MishkaV@users.noreply.github.com>
2025-04-29 20:55:37 +0200

    Make consentTitle24 icon optional (#325)
    * Make consentTitle24 icon optional
    
    * Update api dump

--- fashion-configuration/src/commonMain/kotlin/com/aiuta/fashionsdk/configuration/features/consent/standalone/icons/AiutaConsentStandaloneFeatureIcons.kt
+++ fashion-configuration/src/commonMain/kotlin/com/aiuta/fashionsdk/configuration/features/consent/standalone/icons/AiutaConsentStandaloneFeatureIcons.kt
@@ -3,5 +3,5 @@ package com.aiuta.fashionsdk.configuration.features.consent.standalone.icons
 import com.aiuta.fashionsdk.compose.resources.drawable.AiutaIcon
 
 public interface AiutaConsentStandaloneFeatureIcons {
-    public val consentTitle24: AiutaIcon
+    public val consentTitle24: AiutaIcon?
 }
