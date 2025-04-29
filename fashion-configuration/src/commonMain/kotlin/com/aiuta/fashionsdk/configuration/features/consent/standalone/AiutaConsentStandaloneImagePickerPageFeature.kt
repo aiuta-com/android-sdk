7180367bb34fdba924191b9cc39f4b20d8004908
Mishka Vorozhtsov <54765046+MishkaV@users.noreply.github.com>
2025-04-29 20:39:24 +0200

    Update consent configuration (#323)
    * Update consent configuration
    
    * Update api dump

--- fashion-configuration/src/commonMain/kotlin/com/aiuta/fashionsdk/configuration/features/consent/standalone/AiutaConsentStandaloneImagePickerPageFeature.kt
+++ fashion-configuration/src/commonMain/kotlin/com/aiuta/fashionsdk/configuration/features/consent/standalone/AiutaConsentStandaloneImagePickerPageFeature.kt
@@ -3,19 +3,25 @@ package com.aiuta.fashionsdk.configuration.features.consent
 import com.aiuta.fashionsdk.configuration.features.AiutaFeature
 import com.aiuta.fashionsdk.configuration.features.consent.standalone.data.AiutaConsentStandaloneOnboardingPageFeatureData
 import com.aiuta.fashionsdk.configuration.features.consent.standalone.dataprovider.AiutaConsentStandaloneOnboardingPageFeatureDataProvider
+import com.aiuta.fashionsdk.configuration.features.consent.standalone.icons.AiutaConsentStandaloneFeatureIcons
 import com.aiuta.fashionsdk.configuration.features.consent.standalone.strings.AiutaConsentStandaloneOnboardingPageFeatureStrings
+import com.aiuta.fashionsdk.configuration.features.consent.standalone.styles.AiutaConsentStandaloneFeatureStyles
 import com.aiuta.fashionsdk.configuration.internal.utils.checkNotNullWithDescription
 
 public class AiutaConsentStandaloneImagePickerPageFeature(
     public override val strings: AiutaConsentStandaloneOnboardingPageFeatureStrings,
     public override val data: AiutaConsentStandaloneOnboardingPageFeatureData,
     public override val dataProvider: AiutaConsentStandaloneOnboardingPageFeatureDataProvider?,
+    public override val icons: AiutaConsentStandaloneFeatureIcons,
+    public override val styles: AiutaConsentStandaloneFeatureStyles,
 ) : AiutaConsentStandaloneFeature {
 
     public class Builder : AiutaFeature.Builder {
         public var strings: AiutaConsentStandaloneOnboardingPageFeatureStrings? = null
         public var data: AiutaConsentStandaloneOnboardingPageFeatureData? = null
         public var dataProvider: AiutaConsentStandaloneOnboardingPageFeatureDataProvider? = null
+        public var icons: AiutaConsentStandaloneFeatureIcons? = null
+        public var styles: AiutaConsentStandaloneFeatureStyles? = null
 
         public override fun build(): AiutaConsentStandaloneImagePickerPageFeature {
             val parentClass = "AiutaConsentStandaloneImagePickerPageFeature"
@@ -30,6 +36,14 @@ public class AiutaConsentStandaloneImagePickerPageFeature(
                     property = "data",
                 ),
                 dataProvider = this.dataProvider,
+                icons = icons.checkNotNullWithDescription(
+                    parentClass = parentClass,
+                    property = "icons",
+                ),
+                styles = styles.checkNotNullWithDescription(
+                    parentClass = parentClass,
+                    property = "styles",
+                ),
             )
         }
     }
