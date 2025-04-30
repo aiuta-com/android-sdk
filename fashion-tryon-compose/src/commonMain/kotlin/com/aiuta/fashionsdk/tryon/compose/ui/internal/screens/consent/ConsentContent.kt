ee26065971acd6e93a0d3590e894d9cbe7002576
MishkaV <mavorozhtsov_1@edu.hse.ru>
2025-05-01 00:37:28 +0200

    Update consent with new configuration

--- fashion-tryon-compose/src/commonMain/kotlin/com/aiuta/fashionsdk/tryon/compose/ui/internal/screens/consent/ConsentContent.kt
+++ fashion-tryon-compose/src/commonMain/kotlin/com/aiuta/fashionsdk/tryon/compose/ui/internal/screens/consent/ConsentContent.kt
@@ -1,5 +1,6 @@
 package com.aiuta.fashionsdk.tryon.compose.ui.internal.screens.consent
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Checkbox
 import androidx.compose.material.CheckboxDefaults
 import androidx.compose.material.ExperimentalMaterialApi
@@ -21,11 +23,13 @@ import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.aiuta.fashionsdk.configuration.features.consent.AiutaConsentStandaloneFeature
+import com.aiuta.fashionsdk.configuration.features.consent.models.AiutaConsentType
 import com.aiuta.fashionsdk.internal.analytic.model.AiutaAnalyticPageId
 import com.aiuta.fashionsdk.tryon.compose.domain.models.internal.screen.onboarding.AiutaConsentUiModel
 import com.aiuta.fashionsdk.tryon.compose.ui.internal.analytic.sendPageEvent
@@ -34,6 +38,7 @@ import com.aiuta.fashionsdk.tryon.compose.ui.internal.utils.buildAnnotatedString
 import com.aiuta.fashionsdk.tryon.compose.ui.internal.utils.createCenterAlignmentLine
 import com.aiuta.fashionsdk.tryon.compose.ui.internal.utils.features.strictProvideFeature
 import com.aiuta.fashionsdk.tryon.compose.uikit.composition.LocalTheme
+import com.aiuta.fashionsdk.tryon.compose.uikit.resources.AiutaIcon
 import com.aiuta.fashionsdk.tryon.compose.uikit.utils.clickableUnindicated
 
 @Composable
@@ -57,13 +62,28 @@ internal fun ConsentContent(
             key = "CONSENT_HEADER",
             contentType = { "CONSENT_HEADER_TYPE" },
         ) {
-            Text(
+            Row(
                 modifier = Modifier.fillMaxWidth(),
-                text = consentStandaloneFeature.strings.consentTitle,
-                style = theme.label.typography.titleL,
-                color = theme.color.primary,
-                textAlign = TextAlign.Start,
-            )
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = consentStandaloneFeature.strings.consentTitle,
+                    style = theme.label.typography.titleL,
+                    color = theme.color.primary,
+                    textAlign = TextAlign.Start,
+                )
+
+                consentStandaloneFeature.icons.consentTitle24?.let { consentTitle24 ->
+                    Spacer(Modifier.width(8.dp))
+
+                    AiutaIcon(
+                        modifier = Modifier.size(24.dp),
+                        icon = consentTitle24,
+                        contentDescription = null,
+                    )
+                }
+            }
 
             Spacer(Modifier.height(18.dp))
 
@@ -89,18 +109,32 @@ internal fun ConsentContent(
                 Spacer(Modifier.height(28.dp))
             }
 
+            val consentType = consentModel.consent.type
+            val sharedModifier = Modifier.fillMaxWidth()
+            val finalModifier = if (consentStandaloneFeature.styles.drawBordersAroundConsents) {
+                sharedModifier.border(
+                    color = theme.color.border,
+                    width = 1.dp,
+                    shape = RoundedCornerShape(
+                        topStart = 8.dp.takeIf { index == 0 } ?: 0.dp,
+                        topEnd = 8.dp.takeIf { index == 0 } ?: 0.dp,
+                        bottomStart = 8.dp.takeIf { index == consentStandaloneFeature.data.consents.lastIndex } ?: 0.dp,
+                        bottomEnd = 8.dp.takeIf { index == consentStandaloneFeature.data.consents.lastIndex } ?: 0.dp,
+                    ),
+                )
+            } else {
+                sharedModifier
+            }
+
             AgreePoint(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = finalModifier.padding(16.dp),
                 text = consentModel.consent.consentHtml,
                 isAgreementChecked = consentModel.isObtained,
+                type = consentType,
                 onAgreementCheckedChange = { newState ->
                     onUpdateConsentState(consentModel, newState)
                 },
             )
-
-            if (index != consentStandaloneFeature.data.consents.lastIndex) {
-                Spacer(Modifier.height(40.dp))
-            }
         }
 
         consentStandaloneFeature.strings.consentFooterHtml?.let { footerText ->
@@ -127,6 +161,7 @@ internal fun ConsentContent(
 private fun AgreePoint(
     modifier: Modifier = Modifier,
     text: String,
+    type: AiutaConsentType,
     isAgreementChecked: Boolean,
     onAgreementCheckedChange: (Boolean) -> Unit,
 ) {
@@ -139,25 +174,27 @@ private fun AgreePoint(
     Row(
         modifier = modifier,
     ) {
-        CompositionLocalProvider(LocalRippleConfiguration provides null) {
-            Checkbox(
-                modifier =
-                Modifier
-                    .size(20.dp)
-                    .alignBy(CenterAlignmentLine)
-                    .createCenterAlignmentLine(),
-                checked = isAgreementChecked,
-                onCheckedChange = onAgreementCheckedChange,
-                colors =
-                CheckboxDefaults.colors(
-                    checkedColor = theme.color.brand,
-                    uncheckedColor = theme.color.neutral,
-                    checkmarkColor = theme.color.onDark,
-                ),
-            )
-        }
+        if (type != AiutaConsentType.IMPLICIT_WITHOUT_CHECKBOX) {
+            CompositionLocalProvider(LocalRippleConfiguration provides null) {
+                Checkbox(
+                    modifier = Modifier
+                        .size(20.dp)
+                        .alignBy(CenterAlignmentLine)
+                        .createCenterAlignmentLine(),
+                    checked = isAgreementChecked,
+                    onCheckedChange = onAgreementCheckedChange,
+                    enabled = type != AiutaConsentType.IMPLICIT_WITH_CHECKBOX,
+                    colors =
+                    CheckboxDefaults.colors(
+                        checkedColor = theme.color.brand,
+                        uncheckedColor = theme.color.neutral,
+                        checkmarkColor = theme.color.onDark,
+                    ),
+                )
+            }
 
-        Spacer(Modifier.width(16.dp))
+            Spacer(Modifier.width(16.dp))
+        }
 
         Text(
             modifier =
--- fashion-tryon-compose/src/commonMain/kotlin/com/aiuta/fashionsdk/tryon/compose/ui/internal/screens/consent/controller/ConsentController.kt
+++ fashion-tryon-compose/src/commonMain/kotlin/com/aiuta/fashionsdk/tryon/compose/ui/internal/screens/consent/controller/ConsentController.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import com.aiuta.fashionsdk.configuration.features.consent.AiutaConsentStandaloneFeature
+import com.aiuta.fashionsdk.configuration.features.consent.models.AiutaConsentType
 import com.aiuta.fashionsdk.tryon.compose.domain.models.internal.screen.onboarding.AiutaConsentUiModel
 import kotlin.collections.orEmpty
 import kotlin.collections.toSet
@@ -27,9 +28,11 @@ internal fun AiutaConsentStandaloneFeature.toUiModels(): List<AiutaConsentUiMode
     val obtainedConsentIds = dataProvider?.obtainedConsentsIds?.value.orEmpty().toSet()
     val allConsents = data.consents
     return allConsents.map { consent ->
+        val isHostObtained = obtainedConsentIds.contains(consent.id)
+        val isObtainedType = consent.type == AiutaConsentType.IMPLICIT_WITHOUT_CHECKBOX || consent.type == AiutaConsentType.IMPLICIT_WITH_CHECKBOX
         AiutaConsentUiModel(
             consent = consent,
-            isObtained = obtainedConsentIds.contains(consent.id),
+            isObtained = isHostObtained || isObtainedType,
         )
     }
 }
