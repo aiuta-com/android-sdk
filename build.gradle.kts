ed2f3e2758305f31559b898fcc978b4a11970a49
MishkaV <mavorozhtsov_1@edu.hse.ru>
2025-05-12 20:30:28 +0300

    Fix build of sample

--- build.gradle.kts
+++ build.gradle.kts
@@ -70,7 +70,7 @@ allprojects {
     group = groupId
     version = versionName
 
-    // Target JVM 8.
+    // Target JVM 11.
     tasks.withType<JavaCompile>().configureEach {
         sourceCompatibility = JavaVersion.VERSION_11.toString()
         targetCompatibility = JavaVersion.VERSION_11.toString()
--- fashion-compose-core/src/nonAndroidMain/kotlin/com/aiuta/fashionsdk/compose/core/size/ScreenSize.nonAndroid.kt
+++ fashion-compose-core/src/nonAndroidMain/kotlin/com/aiuta/fashionsdk/compose/core/size/ScreenSize.nonAndroid.kt
@@ -5,15 +5,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalWindowInfo
 
-@Composable
-public actual fun rememberScreenSize(): ScreenSize {
-    val density = LocalDensity.current
-    val windowInfo = LocalWindowInfo.current
-
-    return remember(density, windowInfo) {
-        ScreenSize(
-            heightDp = with(density) { windowInfo.containerSize.height.toDp() },
-            widthDp = with(density) { windowInfo.containerSize.width.toDp() },
-        )
-    }
-}
+//@Composable
+//public actual fun rememberScreenSize(): ScreenSize {
+//    val density = LocalDensity.current
+//    val windowInfo = LocalWindowInfo.current
+//
+//    return remember(density, windowInfo) {
+//        ScreenSize(
+//            heightDp = with(density) { windowInfo.containerSize.height.toDp() },
+//            widthDp = with(density) { windowInfo.containerSize.width.toDp() },
+//        )
+//    }
+//}
