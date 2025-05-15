dea3785aa53b6b814548d21e604b8e93719bc00a
Mishka Vorozhtsov <54765046+MishkaV@users.noreply.github.com>
2025-05-15 11:38:42 +0300

    Update configure event (#340)
    * Update configure event
    
    * Update api dump

--- fashion-configuration/src/commonMain/kotlin/com/aiuta/fashionsdk/configuration/internal/analytic/ConfigureAnalytic.kt
+++ fashion-configuration/src/commonMain/kotlin/com/aiuta/fashionsdk/configuration/internal/analytic/ConfigureAnalytic.kt
@@ -4,8 +4,19 @@ import com.aiuta.fashionsdk.Aiuta
 import com.aiuta.fashionsdk.authentication.ApiKeyAuthenticationStrategy
 import com.aiuta.fashionsdk.authentication.JWTAuthenticationStrategy
 import com.aiuta.fashionsdk.configuration.AiutaConfiguration
+import com.aiuta.fashionsdk.configuration.features.consent.AiutaConsentFeature
+import com.aiuta.fashionsdk.configuration.features.onboarding.AiutaOnboardingFeature
+import com.aiuta.fashionsdk.configuration.features.onboarding.bestresult.AiutaOnboardingBestResultsPageFeature
+import com.aiuta.fashionsdk.configuration.features.picker.camera.AiutaImagePickerCameraFeature
+import com.aiuta.fashionsdk.configuration.features.picker.history.AiutaImagePickerUploadsHistoryFeature
+import com.aiuta.fashionsdk.configuration.features.picker.model.AiutaImagePickerPredefinedModelFeature
 import com.aiuta.fashionsdk.configuration.features.share.AiutaShareFeature
+import com.aiuta.fashionsdk.configuration.features.share.watermark.AiutaShareWatermarkFeature
+import com.aiuta.fashionsdk.configuration.features.tryon.disclaimer.AiutaTryOnFitDisclaimerFeature
+import com.aiuta.fashionsdk.configuration.features.tryon.feedback.AiutaTryOnFeedbackFeature
+import com.aiuta.fashionsdk.configuration.features.tryon.feedback.other.AiutaTryOnFeedbackOtherFeature
 import com.aiuta.fashionsdk.configuration.features.tryon.history.AiutaTryOnGenerationsHistoryFeature
+import com.aiuta.fashionsdk.configuration.features.tryon.other.AiutaTryOnWithOtherPhotoFeature
 import com.aiuta.fashionsdk.configuration.features.welcome.AiutaWelcomeScreenFeature
 import com.aiuta.fashionsdk.configuration.features.wishlist.AiutaWishlistFeature
 import com.aiuta.fashionsdk.configuration.ui.meta.AiutaMode
@@ -14,13 +25,25 @@ import com.aiuta.fashionsdk.internal.analytic.model.ConfigureEvent
 internal fun AiutaConfiguration.sendConfigurationEvent() {
     aiutaAnalytic.sendEvent(
         event = ConfigureEvent(
-            mode = userInterface.styleMetaData?.mode?.toSDKMode() ?: ConfigureEvent.SDKMode.FULL_SCREEN,
+            mode = userInterface.styleMetaData?.mode?.toSDKMode()
+                ?: ConfigureEvent.SDKMode.FULL_SCREEN,
             authenticationType = aiuta.toAuthenticationType(),
-            isHistoryEnable = features.isFeatureInitialize<AiutaTryOnGenerationsHistoryFeature>(),
-            isWishlistAvailable = features.isFeatureInitialize<AiutaWishlistFeature>(),
-            isPreOnboardingAvailable = features.isFeatureInitialize<AiutaWelcomeScreenFeature>(),
-            isShareAvailable = features.isFeatureInitialize<AiutaShareFeature>(),
-            isHostDataProviderEnabled = false, // TODO Refactor property
+            welcomeScreenFeatureEnabled = features.isFeatureInitialize<AiutaWelcomeScreenFeature>(),
+            onboardingFeatureEnabled = features.isFeatureInitialize<AiutaOnboardingFeature>(),
+            onboardingBestResultsPageFeatureEnabled = features.isFeatureInitialize<AiutaOnboardingBestResultsPageFeature>(),
+            consentFeatureEnabled = features.isFeatureInitialize<AiutaConsentFeature>(),
+            imagePickerCameraFeatureEnabled = features.isFeatureInitialize<AiutaImagePickerCameraFeature>(),
+            imagePickerPredefinedModelFeatureEnabled = features.isFeatureInitialize<AiutaImagePickerPredefinedModelFeature>(),
+            imagePickerUploadsHistoryFeatureEnabled = features.isFeatureInitialize<AiutaImagePickerUploadsHistoryFeature>(),
+            tryOnFitDisclaimerFeatureEnabled = features.isFeatureInitialize<AiutaTryOnFitDisclaimerFeature>(),
+            tryOnFeedbackFeatureEnabled = features.isFeatureInitialize<AiutaTryOnFeedbackFeature>(),
+            tryOnFeedbackOtherFeatureEnabled = features.isFeatureInitialize<AiutaTryOnFeedbackOtherFeature>(),
+            tryOnGenerationsHistoryFeatureEnabled = features.isFeatureInitialize<AiutaTryOnGenerationsHistoryFeature>(),
+            tryOnMultiItemFeatureEnabled = false,
+            tryOnWithOtherPhotoFeatureEnabled = features.isFeatureInitialize<AiutaTryOnWithOtherPhotoFeature>(),
+            shareFeatureEnabled = features.isFeatureInitialize<AiutaShareFeature>(),
+            shareWatermarkFeatureEnabled = features.isFeatureInitialize<AiutaShareWatermarkFeature>(),
+            wishlistFeatureEnabled = features.isFeatureInitialize<AiutaWishlistFeature>(),
         ),
     )
 }
