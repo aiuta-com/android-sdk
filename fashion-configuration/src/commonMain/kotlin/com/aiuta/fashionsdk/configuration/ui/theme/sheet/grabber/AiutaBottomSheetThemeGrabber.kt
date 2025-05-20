7e6a9a736ac29b738ad4d23c2b3e195ae05571f0
Mishka Vorozhtsov <54765046+MishkaV@users.noreply.github.com>
2025-05-20 15:44:38 +0500

    Support grabber height in configuration (#348)
    * Support grabber height in configuration
    
    * Update api dump

--- fashion-configuration/src/commonMain/kotlin/com/aiuta/fashionsdk/configuration/ui/theme/sheet/grabber/AiutaBottomSheetThemeGrabber.kt
+++ fashion-configuration/src/commonMain/kotlin/com/aiuta/fashionsdk/configuration/ui/theme/sheet/grabber/AiutaBottomSheetThemeGrabber.kt
@@ -5,10 +5,12 @@ import androidx.compose.ui.unit.dp
 
 public interface AiutaBottomSheetThemeGrabber {
     public val width: Dp
+    public val height: Dp
     public val topPadding: Dp
 
     public class Default : AiutaBottomSheetThemeGrabber {
         override val width: Dp = 36.dp
+        override val height: Dp = 3.dp
         override val topPadding: Dp = 8.dp
     }
 }
