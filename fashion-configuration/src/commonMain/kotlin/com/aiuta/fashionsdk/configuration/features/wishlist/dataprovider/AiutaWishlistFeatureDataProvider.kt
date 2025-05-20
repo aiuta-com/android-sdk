3d239ab783556ca6348eccce3d55a8c16bf165bf
MishkaV <mavorozhtsov_1@edu.hse.ru>
2025-05-20 15:55:37 +0500

    Update naming of product ids

--- fashion-configuration/src/commonMain/kotlin/com/aiuta/fashionsdk/configuration/features/wishlist/dataprovider/AiutaWishlistFeatureDataProvider.kt
+++ fashion-configuration/src/commonMain/kotlin/com/aiuta/fashionsdk/configuration/features/wishlist/dataprovider/AiutaWishlistFeatureDataProvider.kt
@@ -3,6 +3,6 @@ package com.aiuta.fashionsdk.configuration.features.wishlist.dataprovider
 import kotlinx.coroutines.flow.StateFlow
 
 public interface AiutaWishlistFeatureDataProvider {
-    public val wishlistProductsIds: StateFlow<List<String>>
+    public val wishlistProductIds: StateFlow<List<String>>
     public fun setProductInWishlist(productId: String, inWishlist: Boolean)
 }
