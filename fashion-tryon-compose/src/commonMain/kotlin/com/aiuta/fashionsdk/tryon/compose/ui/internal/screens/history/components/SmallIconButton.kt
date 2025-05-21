d7786bbc158db4d5735f9dd3ee6e6eda2f2a7ef9
MishkaV <mavorozhtsov_1@edu.hse.ru>
2025-05-21 15:10:47 +0500

    Support loading share in history

--- fashion-tryon-compose/src/commonMain/kotlin/com/aiuta/fashionsdk/tryon/compose/ui/internal/screens/history/components/SmallIconButton.kt
+++ fashion-tryon-compose/src/commonMain/kotlin/com/aiuta/fashionsdk/tryon/compose/ui/internal/screens/history/components/SmallIconButton.kt
@@ -6,11 +6,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.aiuta.fashionsdk.compose.resources.drawable.AiutaIcon
+import com.aiuta.fashionsdk.tryon.compose.ui.internal.components.icons.AiutaLoadingComponent
 import com.aiuta.fashionsdk.tryon.compose.uikit.composition.LocalTheme
 import com.aiuta.fashionsdk.tryon.compose.uikit.resources.AiutaIcon
 import com.aiuta.fashionsdk.tryon.compose.uikit.utils.clickableUnindicated
@@ -20,6 +23,7 @@ internal fun SmallIconButton(
     modifier: Modifier = Modifier,
     icon: AiutaIcon,
     isActive: Boolean = true,
+    isLoading: Boolean = false,
     onClick: () -> Unit,
 ) {
     val theme = LocalTheme.current
@@ -27,27 +31,32 @@ internal fun SmallIconButton(
         targetValue = if (isActive) theme.color.onDark else theme.color.onDark.copy(alpha = 0.4f),
         label = "backgroundColor",
     )
+    val isClickable = remember(isActive, isLoading) {
+        derivedStateOf { isActive && !isLoading }
+    }
 
     Box(
-        modifier =
-        modifier
+        modifier = modifier
             .size(36.dp)
             .background(
                 color = backgroundColor,
                 shape = CircleShape,
             )
-            .clickableUnindicated {
-                if (isActive) {
-                    onClick()
-                }
+            .clickableUnindicated(enabled = isClickable.value) {
+                onClick()
             },
         contentAlignment = Alignment.Center,
     ) {
-        AiutaIcon(
-            modifier = Modifier.size(24.dp),
-            icon = icon,
-            contentDescription = null,
-            tint = theme.color.primary,
-        )
+        AiutaLoadingComponent(
+            isLoading = isLoading,
+            circleColor = theme.color.primary,
+        ) {
+            AiutaIcon(
+                modifier = Modifier.size(24.dp),
+                icon = icon,
+                contentDescription = null,
+                tint = theme.color.primary,
+            )
+        }
     }
 }
