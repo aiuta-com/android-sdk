138341aefc9dcea0ab475862b59fb0b6303b968c
MishkaV <mavorozhtsov_1@edu.hse.ru>
2025-05-28 19:14:14 +0300

    Add docs for configuration defaults images

--- fashion-configuration-defaults-images/src/commonMain/kotlin/com/aiuta/fashionsdk/configuration/defaults/images/features/onboarding/bestresult/DefaultsAiutaOnboardingBestResultsPageFeatureImages.kt
+++ fashion-configuration-defaults-images/src/commonMain/kotlin/com/aiuta/fashionsdk/configuration/defaults/images/features/onboarding/bestresult/DefaultsAiutaOnboardingBestResultsPageFeatureImages.kt
@@ -9,6 +9,16 @@ import com.aiuta.fashionsdk.compose.resources.drawable.AiutaComposeDrawableResou
 import com.aiuta.fashionsdk.compose.resources.drawable.AiutaDrawableResource
 import com.aiuta.fashionsdk.configuration.features.onboarding.bestresult.images.AiutaOnboardingBestResultsPageFeatureImages
 
+/**
+ * Default implementation of [AiutaOnboardingBestResultsPageFeatureImages].
+ *
+ * This class provides the default image resources for the onboarding best results page,
+ * including examples of good and bad try-on results to help users understand
+ * what makes for a successful try-on experience.
+ *
+ * @property onboardingBestResultsGood List of images showing good examples of try-on results
+ * @property onboardingBestResultsBad List of images showing bad examples of try-on results
+ */
 public class DefaultsAiutaOnboardingBestResultsPageFeatureImages : AiutaOnboardingBestResultsPageFeatureImages {
     override val onboardingBestResultsGood: List<AiutaDrawableResource> = listOf(
         AiutaComposeDrawableResource(Res.drawable.onboarding_good_image_1),
